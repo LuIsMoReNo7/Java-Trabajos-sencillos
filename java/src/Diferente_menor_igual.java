5303473c49b3a836c55c93a5bb995154e9fe5817
LuIsMoReNo7 <1483853@senati.pe>
2023-03-24 11:30:26 -0500

    diferencia, menor o igual

---
+++ java/src/Diferente_menor_igual.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Diferente_menor_igual {
+    public static void main(String[] args) {
+        //ingrsamos el primer dato
+        Scanner dato = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+        int n1 = dato.nextInt(); 
+        //ingresamos el segundo dato
+        System.out.println("Ingrese el segundo numero: ");
+        int n2 = dato.nextInt();
+        //llamamos a if para ver si es diferente
+        if (n1!=n2);{ 
+        System.out.println(n1 + " es diferente que " + n2);
+        } 
+        //llamamos a if para ve si es mayor
+        if (n1>n2);{
+        System.out.println(n1 + " es mayor que " + n2);
+        }   
+        //llamamos a if para ver si es menor o igual
+        if (n1<=n2);{
+        System.out.println(n1 + " es menor o igual que " + n2);
+        } 
+       
+    } 
+}
