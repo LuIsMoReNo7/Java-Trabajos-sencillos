5f3b7db66d580578d6c3a073b7b52836066d46d7
LuIsMoReNo7 <1483853@senati.pe>
2023-03-31 10:19:39 -0500

    cambio

--- java/src/amber.java
+++ java/src/amber.java
@@ -1,5 +1,5 @@
 public class amber {
     public static void main(String[] args){
-        System.out.println("amber de mierda, te voy a matar puto de mierdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, te odio<3");
+        System.out.println("amber xd");
     }
 }
